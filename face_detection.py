9b600e6c8c382abfa4183d9ba80bde748c5d9bc3
Tengku Naim <naimtmas@gmail.com>
2021-04-29 22:11:03 +0800

    Updated image scraper to download better quality images

--- face_detection.py
+++ face_detection.py
@@ -2,19 +2,20 @@ import cv2 as cv
 import numpy as np
 import os
 
-jk_dir = os.path.join(os.getcwd(), "training", "jungkook")
-jk_photos = os.listdir(jk_dir)
+jungkook_dir = os.path.join(os.getcwd(), "training", "jungkook")
+jungkook_photos = os.listdir(jungkook_dir)
 
-img = cv.imread(os.path.join(jk_dir, jk_photos[4]))
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+for photo in jungkook_photos:
+    img = cv.imread(os.path.join(jungkook_dir, photo))
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-haar_cascade = cv.CascadeClassifier(os.path.join(
-    os.getcwd(), "haarcascade_frontalface_default.xml"))
-faces_rect = haar_cascade.detectMultiScale(
-    gray, scaleFactor=1.1, minNeighbors=3)
+    haar_cascade = cv.CascadeClassifier(os.path.join(
+        os.getcwd(), "haarcascade_frontalface_default.xml"))
+    faces_rect = haar_cascade.detectMultiScale(
+        gray, scaleFactor=1.1, minNeighbors=3)
 
-for x, y, w, h in faces_rect:
-    cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 1)
+    for x, y, w, h in faces_rect:
+        cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 1)
 
-cv.imshow("JK Test", img)
-cv.waitKey(0)
+    cv.imshow("JK Test", img)
+    cv.waitKey(0)
--- image_scraper.py
+++ image_scraper.py
@@ -1,11 +1,14 @@
+import urllib.request
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import os
+import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-search_queries = ["jungkook bts portrait", "v bts portrait"]
+search_queries = ["jungkook bts portrait",
+                  "v bts portrait", "jimin bts portrait"]
 training_data_path = os.path.join(os.getcwd(), "training")
 
 for query in search_queries:
@@ -23,9 +26,20 @@ for query in search_queries:
 
     for i in range(1, 10):
         try:
-            picture = f'{query[:query.index(" ")]}({i}).png'
-            save_path = os.path.join(folder_path, picture)
-            driver.find_element_by_xpath(
-                f'//*[@id="islrg"]/div[1]/div[{i}]/a[1]/div[1]/img').screenshot(save_path)
+            save_name = f'{query[:query.index(" ")]}({i}).png'
+            save_path = os.path.join(folder_path, save_name)
+
+            thumbnail = driver.find_element_by_xpath(
+                f'//*[@id="islrg"]/div[1]/div[{i}]/a[1]/div[1]/img')
+            thumbnail.click()
+
+            picture = driver.find_element_by_xpath(
+                '//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div/div[2]/a/img')
+            time.sleep(1)
+            url = picture.get_attribute("src")
+            urllib.request.urlretrieve(url, save_path)
+
         except:
             pass
+
+driver.quit()
