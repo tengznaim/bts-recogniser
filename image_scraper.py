6dc4b1a219060dae418d7a17b5fe2ff9c8d77497
Tengku Naim <naimtmas@gmail.com>
2021-04-30 17:15:20 +0800

    Added scraping for the rest of the members

--- image_scraper.py
+++ image_scraper.py
@@ -11,7 +11,7 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 search_queries = ["jungkook bts portrait",
-                  "v bts portrait", "jimin bts portrait"]
+                  "v bts portrait", "jimin bts portrait", "jin bts portrait", "suga bts portrait", "j-hope bts portrait", "rm bts portrait"]
 training_data_path = os.path.join(os.getcwd(), "training")
 haar_cascade = cv.CascadeClassifier(os.path.join(
     os.getcwd(), "haarcascade_frontalface_default.xml"))
@@ -29,7 +29,12 @@ for query in search_queries:
     search_box.send_keys(query)
     search_box.send_keys(Keys.ENTER)
 
-    for i in range(1, 10):
+    i = 0
+    img_count = 1
+
+    while img_count != 10:
+        i += 1
+        print(i)
         try:
             thumbnail = driver.find_element_by_xpath(
                 f'//*[@id="islrg"]/div[1]/div[{i}]/a[1]/div[1]/img')
@@ -42,7 +47,7 @@ for query in search_queries:
 
             original = io.imread(url)
             detected = cv.cvtColor(imutils.resize(
-                original, height=min(original.shape[0], 600)), cv.COLOR_BGR2RGB)
+                original, height=min(original.shape[0], 600)), cv.COLOR_RGB2BGR)
             gray = cv.cvtColor(detected, cv.COLOR_BGR2GRAY)
             faces_rect = haar_cascade.detectMultiScale(
                 gray, scaleFactor=1.1, minNeighbors=3)
@@ -50,7 +55,7 @@ for query in search_queries:
             if len(faces_rect) == 1:
 
                 for x, y, w, h in faces_rect:
-                    cv.rectangle(detected, (x, y), (x+w, y+h), (255))
+                    cv.rectangle(detected, (x, y), (x+w, y+h), (0, 255, 0))
 
                 cv.imshow("Image", detected)
                 cv.waitKey(0)
@@ -58,9 +63,10 @@ for query in search_queries:
                 user_validation = input("Save this image? y/n:")
 
                 if user_validation == "y":
-                    save_name = f'{query[:query.index(" ")]}({i}).png'
+                    save_name = f'{query[:query.index(" ")]}({img_count}).png'
                     save_path = os.path.join(folder_path, save_name)
                     io.imsave(save_path, original)
+                    img_count += 1
 
         except:
             pass
